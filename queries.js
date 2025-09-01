3c75074e79246c4c27ed306ece4bb890c9541506
Denis S Dujota <denisdujota@gmail.com>
2025-09-01 20:43:49 +0300

    setup initial query

---
+++ queries.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+/* ------------------------------- Starter Code -------------------------------*/
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const mongoose = require('mongoose');
+const Todo = require('./models/todo');
+
+const createTodo = async () => {
+  const todoData = {
+    text: 'Learn JS',
+    isComplete: false,
+  };
+
+  const todo = await Todo.create(todoData);
+  console.log('New todo:', todo);
+};
+
+const runQueries = async () => {
+  console.log('Queries running.');
+  // The functions calls to run queries in our db will go here as we write them.
+  await createTodo();
+};
+
+const connect = async () => {
+  // Connect to MongoDB using the MONGODB_URI specified in our .env file.
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log('Connected to MongoDB');
+
+  // Call the runQueries function, which will eventually hold functions to work
+  // with data in our db.
+  await runQueries();
+
+  // Disconnect our app from MongoDB after our queries run.
+  await mongoose.disconnect();
+  console.log('Disconnected from MongoDB');
+
+  // Close our app, bringing us back to the command line.
+  process.exit();
+};
+
+connect();
+/* ------------------------------ Query Functions -----------------------------*/
